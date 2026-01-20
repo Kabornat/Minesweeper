0f4528ead51096768aebadbd0e7f326a316a37ff
Kabornat <dizuaproject@gmail.com>
2026-01-20 12:30:56 +0300

    Фикс бага не завершающий игру

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -268,6 +268,7 @@ public class Game
                     }
                     return;
                 }
+                GameInfo.GameIsRunning = false;
                 return;
 
             case ConsoleKey.Q:
